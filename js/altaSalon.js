16578c5e7d61aa8d6a7cacbc519d229a387eb00d
Emanuel Comas <emanuelcomas96@gmail.com>
2025-05-28 19:44:13 -0300

    Creacion de 'altaSalon.css', mejora de codigo 'js' para 'edicion' y 'borrado' de salones, creacion de botones de 'edicion' y 'borrado' de salones.

--- js/altaSalon.js
+++ js/altaSalon.js
@@ -12,38 +12,115 @@ document.addEventListener('DOMContentLoaded', () => {
             window.location.href = 'login.html';
         });
     }
-    const form = document.getElementById('formSalon');
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        const nombre = document.getElementById('nombre').value;
-        const valor = document.getElementById('valor').value;
-        const direccion = document.getElementById('direccion').value;
-        const descripcion = document.getElementById('descripcion').value;
-        const salon = {nombre, direccion, descripcion, valor};
-        const salones = JSON.parse(localStorage.getItem('salones')) || [];
-        salones.push(salon);
-        localStorage.setItem('salones', JSON.stringify(salones));//agregamos el elemento salones al local storage
-
-
-        alert(` El nuevo salon tiene los siguientes atributos nombre: ${nombre} --direccion: ${direccion} --descripcion: ${descripcion} --valor: ${valor}`);
-        form.reset();//Limpia el formulario
-        
-        mostrarSalones();
-    });
+    
     mostrarSalones();
 });
+
+
 function mostrarSalones(){
     const tablaBody = document.querySelector('#tablaSalones tbody');
     tablaBody.innerHTML = ''; //Limpia el contenido previo
     const salones = JSON.parse(localStorage.getItem('salones')) || [];
-    salones.forEach((salon) =>{
+    salones.forEach((salon, index) =>{
         const fila = document.createElement('tr');
         fila.innerHTML = `
         <td>${salon.nombre}</td>
         <td>${salon.direccion}</td>
         <td>${salon.descripcion}</td>
         <td>${salon.valor}</td>
+        <td>
+            <button class="editar-btn" data-index="${index}">Editar</button>
+            <button class="borrar-btn" data-index="${index}">Borrar</button>
+        </td>
         `;
         tablaBody.appendChild(fila);
     })
+
+    // Eventos para los botnes 'editar' y 'borrar'
+    document.querySelectorAll('.borrar-btn').forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            const index = e.target.getAttribute('data-index');
+            borrarSalon(index)
+        });
+    });
+
+    document.querySelectorAll('.editar-btn').forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            const index = e.target.getAttribute('data-index');
+            editarSalon(index)
+        });
+    });
 }
+
+
+function borrarSalon(index){
+    const salones = JSON.parse(localStorage.getItem('salones')) || [];
+    salones.splice(index, 1);
+    localStorage.setItem('salones', JSON.stringify(salones));
+    // Vuelve a mostrar la tabla actualizada.
+    mostrarSalones()
+}
+
+
+// Variable global para control y poder editar los salones.
+let indiceEdicion = null;
+// Actualización de los cambios.
+const form = document.getElementById('formSalon');
+const botonSubmit = form.querySelector('button[type="submit"]');
+
+function editarSalon(index){
+    const salones = JSON.parse(localStorage.getItem('salones')) || [];
+    const salon = salones[index];
+
+    // Llena el formulario con los datos existentes
+    document.getElementById('nombre').value = salon.nombre;
+    document.getElementById('direccion').value = salon.direccion;
+    document.getElementById('descripcion').value = salon.descripcion;
+    document.getElementById('valor').value = salon.valor;
+
+    indiceEdicion = index;
+
+    // Se cambia el texto del boton.
+    botonSubmit.textContent = 'Actualizar';
+
+    // Focus al compo nombre al editar
+    document.getElementById('nombre').focus();
+
+}
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const nombre = document.getElementById('nombre').value;
+    const direccion = document.getElementById('direccion').value;
+    const descripcion = document.getElementById('descripcion').value;
+    const valor = document.getElementById('valor').value;
+
+
+    // Validación para evitar lso campos vacios.
+    if (!nombre || !direccion || !descripcion || !valor){
+        alert("Completa los campos.");
+        return;
+    }
+
+
+    const salones = JSON.parse(localStorage.getItem('salones')) || [];
+        
+    if (indiceEdicion !== null){
+        // Aqui se edita
+        salones[indiceEdicion] = {nombre, direccion, descripcion, valor};
+        indiceEdicion = null;
+        // Se restaura el texto del boton a 'Guardar salón'.
+        botonSubmit.textContent = 'Guardar salón'
+    }
+    else{
+        // Aqui se agrega un nuevo salon
+        salones.push({nombre, direccion, descripcion, valor});
+    }
+    
+    localStorage.setItem('salones', JSON.stringify(salones));
+
+    form.reset();
+    mostrarSalones();
+
+});
