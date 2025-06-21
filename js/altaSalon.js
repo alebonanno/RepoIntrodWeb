16a97656ac7633379775f44a227838001413c2fc
alebonanno <sayanigrafica@gmail.com>
2025-06-21 18:05:45 -0300

    conflicto resuelto

--- js/altaSalon.js
+++ js/altaSalon.js
@@ -53,29 +53,6 @@ function mostrarSalones(){
     const tablaBody = document.querySelector('#tablaSalones tbody');
     tablaBody.innerHTML = ''; //Limpia el contenido previo
     const salones = JSON.parse(localStorage.getItem('salones')) || [];
-<<<<<<< HEAD
-    // Verificación por consola.
-    console.log("Salones cargados: ", salones);
-    // Para verificar despues si es un suario logueado (Admin) o usuario final.
-    // Se define de nuevo por que afecta solo a la funcion.
-    const userLogueado = sessionStorage.getItem('usuario')
-
-    salones.forEach((salon, index) =>{
-        const fila = document.createElement('tr');
-        fila.innerHTML = `
-        <td class='centrado-vertical'>${salon.nombre}</td>
-        <td>
-            <img src='./images/Salon_comedor.jpg' alt='Imagen del salón' style='width: 100px; height: auto;'>
-        </td>
-        <td class='centrado-vertical'>${salon.direccion}</td>
-        <td class='centrado-vertical'>${salon.descripcion}</td>
-        <td class='centrado-vertical'>${salon.valor}</td>
-        <td class="d-flex justify-content-center gap-2 py-4">
-            ${userLogueado ? ` 
-            <button class="btn btn-primary editar-btn" data-index="${index}">Editar</button>
-            <button class="btn btn-danger borrar-btn" data-index="${index}">Borrar</button>
-            `: '<button class="btn btn-primary comprar-btn" data-index="${index}">Comprar</button>'}
-=======
     salones.forEach((salon,index) =>{  //index es una posición unica en el array
         const fila = document.createElement('tr');
         fila.innerHTML = `
@@ -86,7 +63,6 @@ function mostrarSalones(){
         <td>
             <button class="btn btn-sm btn-warning me-2" onclick="editarSalon(${index})">Editar</button>
             <button class="btn btn-sm btn-danger" onclick="eliminarSalon(${index})">Eliminar</button>
->>>>>>> 4taEntrega
         </td>
         `;
         tablaBody.appendChild(fila);
